771e4a17f6bf4a88b7b84dc22b417bab743194a0
Joseph Billings <josephbillings.gb@gmail.com>
2024-06-25 13:16:27 -0600

    created inputs labeled Picture: and Name: , and a button 'add friend' in the App function; added states for friends, picture, and name

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div>Hello</div>;
+  const [friends, setFriends] = useState([]);
+  const [picture, setPicture] = useState("");
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <label htmlFor="imgUrl">Picture:</label>
+      <input type="text" name="imgUrl" id="imgUrl" />
+      <label htmlFor="name">Name:</label>
+      <input type="text" name="name" id="name" />
+      <button type="button">Add Friend</button>
+    </div>
+  );
 }
