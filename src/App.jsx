a7eac37a639415c5a5b15965a56bd1a01d02e676
Joseph Billings <josephbillings.gb@gmail.com>
2024-06-25 13:28:20 -0600

    added onChange handlers for picture and name inputs

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,19 @@ export default function App() {
   return (
     <div>
       <label htmlFor="imgUrl">Picture:</label>
-      <input type="text" name="imgUrl" id="imgUrl" />
+      <input
+        id="imgUrl"
+        type="text"
+        value={picture}
+        onChange={(e) => setPicture(e.target.value)}
+      />
       <label htmlFor="name">Name:</label>
-      <input type="text" name="name" id="name" />
+      <input
+        id="name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button type="button">Add Friend</button>
     </div>
   );
